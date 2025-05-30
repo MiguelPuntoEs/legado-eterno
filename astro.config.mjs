018fa34bf424bab7dbe13e0dd98c07a5f3d819eb
Miguel González <miguel.gc@me.com>
2025-05-30 09:19:19 +0200

    fix: corrected URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://www.legadoeterno.com',
+    site: 'https://www.tulegadoeterno.com',
     vite: {
         plugins: [tailwindcss()],
     },
